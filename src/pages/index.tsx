f7470f7f32e29a9b6f056f83502e29779c924608
João Queiroz <joao.queiroz@hydradev.io>
2023-04-16 11:55:29 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Scheduler</title>
+        <meta name="description" content="2 weeks app challenge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div></div>
+    </>
+  );
+}
