397f04346e1de0baee8572c38d5bfafe035e339b
João Queiroz <joao.queiroz@hydradev.io>
2023-04-25 20:05:23 +0100

    Display tasks in frontend

--- src/components/AgendaOverviewCard.tsx
+++ src/components/AgendaOverviewCard.tsx
@@ -1,21 +1,31 @@
 import { IAgenda } from "@/types/agenda";
 import React from "react";
 
+const MAX_DISPLAYED_TASKS = 3;
+
 interface IProps {
   agenda: IAgenda;
 }
 
 const AgendaOverviewCard: React.FC<IProps> = ({ agenda }) => {
+  const tasksList = (agenda: IAgenda) => {
+    if (agenda.tasks.length <= MAX_DISPLAYED_TASKS) {
+      return agenda.tasks.map((task) => <li id={task.id}>{task.name}</li>);
+    }
+
+    return [
+      ...agenda.tasks
+        .slice(0, MAX_DISPLAYED_TASKS - 1)
+        .map((task) => <li id={task.id}>{task.name}</li>),
+      <li>{agenda.tasks.length - MAX_DISPLAYED_TASKS + 1} more tasks</li>,
+    ];
+  };
+
   return (
     <div className="bg-white rounded-lg shadow-lg overflow-hidden">
       <div className="p-6">
         <h2 className="text-2xl font-bold mb-4">{agenda.name}</h2>
-        {/* <ul className="list-disc ml-6 mb-4">
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-          <li>Item 4</li>
-        </ul> */}
+        <ul className="list-disc ml-6 mb-4">{tasksList(agenda)}</ul>
         <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
           Show More
         </button>
