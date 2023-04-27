4235a8010489fa0fe4a849500a27823364b3743d
João Queiroz <joao.queiroz@hydradev.io>
2023-04-27 16:44:04 +0100

    Add overflow in text

--- src/components/AgendaOverviewCard.tsx
+++ src/components/AgendaOverviewCard.tsx
@@ -26,13 +26,19 @@ const AgendaOverviewCard: React.FC<IProps> = ({ agenda }) => {
 
   const tasksList = (agenda: IAgenda) => {
     if (agenda.tasks.length <= MAX_DISPLAYED_TASKS) {
-      return agenda.tasks.map((task) => <li key={task.id}>{task.name}</li>);
+      return agenda.tasks.map((task) => (
+        <li key={task.id} className="truncate">
+          {task.name}
+        </li>
+      ));
     }
 
     return [
-      ...agenda.tasks
-        .slice(0, MAX_DISPLAYED_TASKS - 1)
-        .map((task) => <li key={task.id}>{task.name}</li>),
+      ...agenda.tasks.slice(0, MAX_DISPLAYED_TASKS - 1).map((task) => (
+        <li key={task.id} className="truncate">
+          {task.name}
+        </li>
+      )),
       <li key="more">
         {agenda.tasks.length - MAX_DISPLAYED_TASKS + 1} more tasks
       </li>,
@@ -43,14 +49,16 @@ const AgendaOverviewCard: React.FC<IProps> = ({ agenda }) => {
 
   return (
     <div
-      className="bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer h-56"
+      className="bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer h-56 pb-6 pl-3"
       onClick={handleClickAgenda}
     >
       <div className="p-6">
-        <h2 className="text-2xl font-bold">{agenda.name}</h2>
-        <div className="font-light">owner: {agenda.owner.name}</div>
+        <h2 className="text-2xl font-bold truncate">{agenda.name}</h2>
+        <div className="font-light truncate">owner: {agenda.owner.name}</div>
         <div className="font-light mb-4">Time offset (hours): {timeDiff}</div>
-        <ul className="list-disc ml-6 mb-4">{tasksList(agenda)}</ul>
+        <ul className="list-disc mb-4 list-inside -ml-6 pl-8">
+          {tasksList(agenda)}
+        </ul>
       </div>
     </div>
   );
